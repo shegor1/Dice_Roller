6e4d7657f8fabfb780f0432852a56c0e707e4446
Egor Shel <shegor1997@gmail.com>
2020-12-10 01:39:35 +0500

    Really cool view binding

--- app/src/main/java/com/shegor/diceroller/MainActivity.kt
+++ app/src/main/java/com/shegor/diceroller/MainActivity.kt
@@ -2,10 +2,14 @@ package com.shegor.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shegor.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.result.text = "5"
+        setContentView(binding.root)
+
     }
 }
