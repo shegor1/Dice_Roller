88140a9c071d8ae840244922b086df2cd402f4d4
Egor Shel <shegor1997@gmail.com>
2020-12-10 13:29:53 +0500

    Strange but workung solve of the layout shift problem + toast and random

--- app/src/main/java/com/shegor/diceroller/MainActivity.kt
+++ app/src/main/java/com/shegor/diceroller/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.shegor.diceroller
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.shegor.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.result.text = "5"
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.bind(findViewById(R.id.root_layout))
+        binding.rollButton.setOnClickListener {
+            Toast.makeText(this, "", Toast.LENGTH_SHORT)
+            rollDice()
+        }
     }
+    fun rollDice(){
+        binding.resultTextView.text = (Random.nextInt(6) + 1).toString()
+    }
+
 }
